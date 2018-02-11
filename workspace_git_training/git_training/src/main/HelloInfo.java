f8d62012b28f4a47b1d3189589e153b8c0d92d70
miyake <miyake@p-branch.co.jp>
2018-02-11 15:16:28 +0900

    HelloInfoクラスにコンストラクタとメソッドを追加しました

--- workspace_git_training/git_training/src/main/HelloInfo.java
+++ workspace_git_training/git_training/src/main/HelloInfo.java
@@ -4,4 +4,19 @@ public class HelloInfo
 {
     private String name;
     private int age;
+
+    public HelloInfo(String name, int age)
+    {
+    	this.name = name;
+    	this.age = age;
+    }
+
+    public String getName()
+    {
+    	return name;
+    }
+    public int age()
+    {
+    	return age;
+    }
 }
